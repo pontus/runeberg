d636be9d7b439d4ab4d9ca278efb308619c8f11d
Pontus Freyhult <pontus_git_commit@soua.net>
2012-05-18 02:17:52 +0200

    Initial working prototype.

---
+++ runepub.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+set -e
+
+work="$1"
+
+runebergid="http://runeberg.org/$work/"
+
+rm -rf /tmp/runepub/ws*
+mkdir -p /tmp/runepub/ws$$/work/META-INF
+cd /tmp/runepub/ws$$
+wget -O "$work.zip" "http://runeberg.org/download.pl?mode=txtzip&work=$work"
+unzip "$work".zip
+
+grep ^LICENSE: Metadata && echo License found in Metadata file, is this really free? && exit 1
+
+charset=l1
+language=sv
+#TODO: check for other languages
+
+if grep ^CHARSET: Metadata; then
+    charset=`grep ^CHARSET: Metadata | sed -e 's/.*: *//'`
+fi
+
+authors=`grep ^AUTHORKEY: Metadata | sed -e 's/.*: *//'`
+year=`grep ^PUBLISHING_YEAR: Metadata | sed -e 's/.*: *//'`
+titlename=`grep ^TITLE: Metadata | sed -e 's/.*: *//'  | recode l1..utf8`
+
+for p in $authors; do
+  authorname="$authorname "`curl  http://runeberg.org/authors/$p.html | grep '<title>' | sed -e 's/<.*>//g' | recode l1..utf8`
+done
+
+echo $authorname $titlename
+
+
+# Set up some general stuff for the epub
+
+
+    echo -n 'application/epub+zip' > work/mimetype
+
+    cat - >work/META-INF/container.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0">
+   <rootfiles>
+      <rootfile full-path="book.opf" media-type="application/oebps-package+xml"/>
+   </rootfiles>
+</container>
+EOF
+
+    cat ->work/book.ncx <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN" 
+   "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+ 
+<ncx version="2005-1" xml:lang="en"
+     xmlns="http://www.daisy.org/z3986/2005/ncx/">
+ 
+<head>
+    <!-- The following four metadata items are required for all NCX documents,
+         including those conforming to the relaxed constraints of OPS 2.0 -->
+ 
+   <meta name="dtb:uid" content="$uid"/> <!-- same as in .opf -->
+   <meta name="dtb:depth" content="1"/> <!-- 1 or higher -->
+   <meta name="dtb:totalPageCount" content="0"/> <!-- must be 0 -->
+   <meta name="dtb:maxPageNumber" content="0"/> <!-- must be 0 -->
+</head>
+ 
+<docTitle>
+   <text>$titlename</text>
+</docTitle>
+ 
+<docAuthor>   
+   <text>$authorname</text>
+</docAuthor>
+ 
+<navMap>
+EOF
+
+
+
+
+
+cat ->work/book.opf <<EOF
+<?xml version="1.0"?>
+<package version="2.0" xmlns="http://www.idpf.org/2007/opf" unique-identifier="BookId">
+ 
+<metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
+   <dc:title>$titlename</dc:title>
+   <dc:language>$language</dc:language>
+   <dc:identifier id="BookId">$runebergid</dc:identifier>
+   <dc:creator opf:role="aut">$authorname</dc:creator>
+</metadata>
+
+<manifest>
+EOF
+
+
+
+
+
+
+
+
+num=0;
+
+# TODO: Handle things that aren't collections
+
+egrep -v '(^#|^-)' Articles.lst | while read l; do
+  htmlfile=`echo $l | sed -e 's/|.*//'`
+  chaptername=`echo $l | sed -e 's/.*|\([^|]*\)|.*/\1/' | recode $charset..utf8`
+  pages=`echo $l | sed -e 's/.*|//'`
+  echo $htmlfile $chaptername $pages
+  
+
+  if [ ! -f "$htmlfile" ]; then
+      # We need to build a htmlfile
+
+      htmlfile="$chaptername.html"
+
+      rm -f "$htmlfile"
+      echo "<h2>$chaptername</h2><p>" >>"$htmlfile"
+      for p in $pages; do
+	  # Expand ranges, if needed. Assumes increasing ranges. Assume 4 digits in filenames for now.
+          curpages=`seq -f %04g "${p%-*}" "${p#*-}"`
+
+	  for q in $curpages; do
+	      # TODO: Actually detect if we have headers/footers to remove instead of assuming so.
+	      cat Pages/$q.txt | tail -n +3 | head -n -2| recode $charset..utf8 | \
+		  sed -e 's/<td r>/<td>/g' | \
+		  sed -e 's/<td [^>]* c>/<td>/g' | \
+		  sed -e 's,^\s*$,</p><p>,' >>"$htmlfile"
+	  done
+      done
+      echo '</p>' >> "$htmlfile"
+      
+
+    num=$((num+1))
+      tidy -n -asxhtml -utf8 "$htmlfile"  >"$htmlfile.tmp" || true
+      mv "$htmlfile.tmp" "work/chapter$num.html"
+
+  
+
+      # Add to index
+
+
+    cat ->>work/book.ncx <<EOF
+   <navPoint class="chapter" id="chapter$num" playOrder="$num">
+      <navLabel><text>$chaptername</text></navLabel>
+      <content src="chapter$num.html"/>
+    </navPoint>
+EOF
+
+
+    cat - >>work/book.opf <<EOF
+   <item id="chapter$num"
+         href="chapter$num.html"
+         media-type="application/xhtml+xml"/>
+EOF
+
+
+    echo "<itemref idref=\"chapter$num\" /> " >> work/itemrefs
+
+
+    fi
+done
+
+# Close index
+
+    cat ->>work/book.ncx <<EOF
+</navMap>
+</ncx>
+EOF
+
+
+itemrefs=`cat work/itemrefs`
+rm -f work/itemrefs
+
+cat ->>work/book.opf <<EOF
+
+   <item id="ncx"
+         href="book.ncx"
+         media-type="application/x-dtbncx+xml"/>
+</manifest>
+ 
+<spine toc="ncx">
+$itemrefs
+</spine>
+</package>
+EOF
+
+cd work
+
+rm -f ../$work.epub
+zip -X -9 ../$work.epub mimetype
+zip -X -9 -u -r ../$work.epub *

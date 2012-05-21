937ae719551078061bc4467e7f5a81b7cfab00da
Pontus Freyhult <pontus_git_commit@soua.net>
2012-05-21 12:38:28 +0200

    Fixups for publishing

--- special/annasvard.sh
+++ special/annasvard.sh
@@ -1,1 +1,22 @@
 isbn=9789175350004
+
+cat - >work/chapter2.html <<EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Anna Svärd av Selma Lagerlöf</title>
+</head>
+<body>
+<div style="text-align: center">
+<h1>ANNA SVÄRD</h1>
+</div>
+<hr />
+<h2>Förord till den elektroniska utgåvan</h2>
+<p>Romanen <i>Anna Svärd</i> utkom första gången 1928. I oktober
+2011 digitaliserades denna tryckning från 1978 av Skrifter av Selma
+Lagerlöf, i tolv volymer, där den utgör sista volymen.</p>
+</body>
+</html>
+
+EOF

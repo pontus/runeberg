4f87571070be10be0b709b19aa311b5dcc3ffc80
Pontus Freyhult <pontus_git_commit@soua.net>
2012-05-26 21:54:09 +0200

    Various general text fixes.

--- htmlwash.sh
+++ htmlwash.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
-sed -e 's/align="center"/style="text-align: center"/g' -e 's,Skrifter av <a href="/authors.[^>]*>[^>]*>,,g'
+sed -e 's/align="center"/style="text-align: center"/g' \
+    -e 's/align=center/style="text-align: center"/g'  \
+    -e 's,a href="/,a href="http://www.runeberg.org/,ig' \
+    -e "s,a href='/,a href='http://www.runeberg.org/,ig" \
+    -e 's,<sp>,<em>,ig' \
+    -e 's,</sp>,</em>,ig'  \
+    -e 's,<font[^>]*>\([^<]*\)</font>,<span style="text-size: 200%">\1</span>,ig' \
+    -e 's,<br />,<span style="clear: both" />,ig' \
+    -e 's,<blockquote>,<blockquote><div>,ig' \
+    -e 's,</blockquote>,</div></blockquote>,ig' \
+    -e 's, align="*right"*, style="float: right",ig' \
+    -e 's, hspace="*\([0-9]*\)"*, style="margin-right: \1",ig' \
+    -e 's,<blockquote>,<blockquote><div>,ig' \
+    -e 's,</blockquote>,</div></blockquote>,ig' \
+    -e 's,\(<img [^>]*/>\),<div>\1</div>,ig' \
+    -e '/alt=/ ! s,<img ,<img alt="" ,ig' \
+    -e 's,\(<img[^>]*src="\)/,\1,ig' \
+    -e 's,<comment>,<!-- ,ig' \
+    -e 's,</comment>, -->,ig' \
+    -e 's,<center>,<div style="text-align: center">,ig' \
+    -e 's,</center>,</div>,ig' \
+    -e 's,^<p<,<p>,ig'
+
 
--- textwash.sh
+++ textwash.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sed -e 's/<td r>/<td>/g' -e 's/<td [^>]* c>/<td>/g' -e 's,^\s*$,</p><p>,' 
+sed -e 's/<td r>/<td>/g' \
+    -e 's/<td [^>]* c>/<td>/g' \
+    -e 's,^\s*$,</p><p>,ig'  \
+    -e 's,<sp>,<em>,ig' \
+    -e 's,</sp>,</em>,ig'
